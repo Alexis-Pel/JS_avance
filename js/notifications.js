fe178694de100fada4f2749c6f077bf77472fddf
Alexis-Pel <alexispelissier2002@gmail.com>
2021-03-03 10:32:09 +0100

    ajout d'une fonction de notification d'ajout dans le panier

---
+++ js/notifications.js
@@ -0,0 +1,32 @@
+let allButtons = document.querySelectorAll('a[class="add-to-cart"]');
+let notif_box = document.querySelector("element[id='notification_container']")
+let compteur = 0;
+
+for (var i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener('click', addNotification);
+}
+
+/**
+ * Fonction qui créer des notifications,
+ * les affiche dans le container notif_box,
+ * puis les supprime au bout de 2 secondes,
+ * compteur qui permet de ne pas submerger la page de notifications
+ */
+function addNotification(){
+    if(compteur < 3){
+        let content_box = document.createElement('element');
+        content_box.className = 'content';
+        let message = document.createElement('p');
+        message.innerHTML="L'élément a été ajouté au panier";
+        content_box.appendChild(message);
+        notif_box.appendChild(content_box);
+        compteur++;
+        setTimeout(function() {
+            content_box.style.opacity = '0';
+            setTimeout(function() {
+                notif_box.removeChild(content_box);
+                compteur--;
+            }, 1000)
+        }, 1200)
+    }
+}
