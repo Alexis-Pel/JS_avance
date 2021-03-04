1f0830c7ebf0ab62a555d196e95625ede270ff05
Alexis RABILLER <alexis.rabiller@edu.itescia.fr>
2021-03-04 11:43:18 +0100

    timer finis

--- js/cours.js
+++ js/cours.js
@@ -1,4 +1,5 @@
 const bodiv = document.querySelector('div.courses__container');
+
 //boucle pour afficher les articles
 let i = 1;
 for(const propriaty in COURSES){
@@ -6,14 +7,8 @@ for(const propriaty in COURSES){
   console.log(COURSES[i].id);
   i++;
 }
-/*for(let i = 1 ; i <= 100; i++){
-  if(COURSES[i] == null){
-    break;
-  }
-  else{
-    
-  }
-}*/
+timerDiscount();
+
 //fonction pour créer un article
 function createCard(i){
   
@@ -42,9 +37,10 @@ function createCard(i){
   let rate = document.createElement('img');
   rate.src = "img/rates.png";
   let p_span2 = document.createElement('p');
+  p_span2.className = "p_span2";
   let price = document.createElement('span');
   price.className = "price";
-  price.textContent = `${COURSES_initial_price} €`;
+  price.textContent = `${COURSES_initial_price} € `;
   let discount = document.createElement('span');
   discount.className = "discount";
   discount.textContent = `${COURSES_price} €`;
@@ -56,7 +52,7 @@ function createCard(i){
   let a = document.createElement('a');
   a.href = '#';
   a.className = 'add-to-cart';
-  a.dataset.id = `${COURSES_id}`;
+  a.id = `${COURSES_id}`;
   a.textContent = `Ajouter au panier`;
   
   //#appendChild des createElements ci-dessus#//
@@ -81,8 +77,58 @@ function createCard(i){
   //<p>/p_span
   p_span.appendChild(stock);
 }
+//function pour mettre un timer sur le prix des articles 
+function timerDiscount(){
+  //modifie le prix pour le prix soldé
+  let m = 0;
+  let s = 0;
+  let ms = 0;
 
+  const p_doc = document.querySelectorAll('p.p_span2');
+  const span_d = document.querySelectorAll('span.discount');
+  const span_p = document.querySelectorAll('span.price');
+  for(let i = 0 ; i < p_doc.length ; i++ ){
+    span_d[i].style.color = "transparent";
+  }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+  let randomC = getRandomInt(p_doc.length);
+  console.log(randomC);
+  span_d[randomC].style.color = "black";
+  span_p[randomC].style.color = "red";
+  span_p[randomC].style.textDecoration = "line-through";
 
+  let newElement = document.createElement("span");
+  newElement.textContent = `${m}h ${s}m ${ms}s`;
+  p_doc[randomC].appendChild(newElement);
+
+  let totalSeconds = 0.25 * 60;
+  
+  let interval = setInterval(Timer_Tick,1000);
+  
+  function Timer_Tick(){
+
+  if (totalSeconds > 0) // S'il reste du temps ...
+  {
+    totalSeconds--; // Décrémenter le nombre total de secondes
+
+  let seconds = totalSeconds % 60; //Recalculer les valeurs de minuterie et afficher ensuite
+  let minutes = Math.floor(totalSeconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  newElement.textContent = `${hours}h ${minutes}m ${seconds}s`;
+  }
+  if(totalSeconds <= 0){
+    clearInterval(interval);
+    p_doc[randomC].removeChild(newElement);
+    span_d[randomC].style.color = "transparent";
+    span_p[randomC].style.color = "black";
+    span_p[randomC].style.textDecoration = "none";
+  }
+ }
+}
 
 /* <div class="courses__container">
     <div class="course__item">
